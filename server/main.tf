12a50a07b682e9d2341b6fb6041f9c1779a1d2dc
robertpountney92 <50262807+robertpountney92@users.noreply.github.com>
2020-01-20 15:17:33 +0000

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -13,20 +13,20 @@ variable "num_webs" {
 	default = 3
 }
 resource "aws_instance" "web" {
-  count = "${var.num_webs}"
-	ami                    = "${var.ami}"
+  count 		 = "${var.num_webs}"
+  ami                    = "${var.ami}"
   instance_type          = "${var.instance_type}"
   subnet_id              = "${var.subnet_id}"
   vpc_security_group_ids = ["${var.vpc_security_group_id}"]
 	key_name 							 = "${aws_key_pair.training.key_name}"
 	
   tags = {
-    Name       = "Rob Pountney ${count.index+1}/${var.num_webs}"
-    training   = "Academy"
-    "Identity" = "${var.Identity}"
+    "Name"       = "Rob Pountney ${count.index+1}/${var.num_webs}"
+    "training"   = "Academy"
+    "Identity"   = "${var.Identity}"
   }
 	
-	connection {
+  connection {
     user     = "ubuntu"
     private_key = "${file("~/.ssh/id_rsa")}"
   }
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
     destination = "/tmp/"
   }
 
-	provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
       "sudo sh /tmp/assets/setup-web.sh"
     ]
